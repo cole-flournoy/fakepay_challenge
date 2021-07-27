6fdc22fe109810b00ec6483bc8bcddfe4d8efdfe
Cole Flournoy <flournoy.cole@gmail.com>
2021-07-27 19:21:13 -0400

    Replace test token with real fakepay token

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 require 'net/https'
 require 'pry'
 require 'json'
+require 'dotenv/load'
 
 class CustomersController < ApplicationController
 
@@ -25,9 +26,11 @@ def purchase_post_req
   req = Net::HTTP::Post.new(uri)
   req.body = {"amount": "1000", "card_number": "4242424242424242", "cvv": "123", "expiration_month": "01", "expiration_year": "2024", "zip_code": "10045"}.to_json
   req["Content-Type"] = "application/json"
-  req["Authorization"] = "Token token=cfc65ad1671a865ba28c4911126ce9"
-
+  req["Authorization"] = "Token token=#{ENV["FAKEPAY_TOKEN"]}"
+  binding.pry
   response = http.start{|http| http.request(req)}
   token = JSON.parse(response.body)['token']
   puts JSON.parse(response.body)
 end
+
+# purchase_post_req

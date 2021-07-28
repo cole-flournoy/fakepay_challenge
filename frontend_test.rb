5f487bb716c1c3915317252ccbd856bd95d6c68e
Cole Flournoy <flournoy.cole@gmail.com>
2021-07-27 22:48:45 -0400

    Update fake frontend for new adn existing users

--- frontend_test.rb
+++ frontend_test.rb
@@ -1,16 +1,51 @@
 require 'net/http'
 require 'json'
 
-def rails_api_test
+def new_user_request
   uri = URI.parse('http://127.0.0.1:3000/')
   
   http = Net::HTTP.new(uri.host, uri.port)
   req = Net::HTTP::Post.new(uri)
-  req.body = {'customer': {'id': '12', 'first_name': 'Cole'}}.to_json
+  req.body = {
+    'customer': {
+      'id': nil, 
+      'first_name': 'Cole',
+      'last_name': 'Flournoy', 
+      'subscription': {
+        'plan': 'silver' 
+      }, 
+      'payment': {
+        'card_number': '4242424242424242',
+        'cvv': '123',
+        'expiration_month': '01',
+        'expiration_year': '2024',
+        'zip_code': '10045'
+      }
+    }
+  }.to_json
   req["Content-Type"] = "application/json"
 
   response = http.start{|http| http.request(req)}
-  # puts JSON.parse(response.body)
 end
 
-rails_api_test
+def existing_user_request
+  uri = URI.parse('http://127.0.0.1:3000/')
+  
+  http = Net::HTTP.new(uri.host, uri.port)
+  req = Net::HTTP::Post.new(uri)
+  req.body = {
+    'customer': {
+      'id': 1,  
+      'subscription': {
+        'plan': 'bronze' 
+      } 
+    }
+  }.to_json
+  req["Content-Type"] = "application/json"
+
+  response = http.start{|http| http.request(req)}
+end
+
+
+# new_user_request
+# existing_user_request

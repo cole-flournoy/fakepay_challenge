84052010ac7e2bd0247faab903b00442b78a1089
Cole Flournoy <flournoy.cole@gmail.com>
2021-07-28 14:26:02 -0400

    Update serializers to send related objects

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,4 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :token
+  attributes :id, :first_name, :last_name, :token, :subscriptions
   has_many :subscriptions
 end
